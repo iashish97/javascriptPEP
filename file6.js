db459e434c0c782affd4ddac0fd51ea5ebeef1a7
iashish97 <ashishmohan27622@gmail.com>
2026-02-11 15:08:05 +0530

    first commit

---
+++ file6.js
@@ -0,0 +1,25 @@
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+//     if(i==3){
+//         break
+//     }
+// }
+
+
+// for(let i=1;i<=5;i++){
+//     if(i==3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+//using for loop 5 10 15 ...30
+// for(let i=1;i<=30;i++){
+//     if(i%5==0){
+//         console.log(i)
+//     }
+// }
---
+++ file7.js
@@ -0,0 +1,40 @@
+// function greet(){
+//     console.log("hello world")
+// }
+
+// greet()
+// greet()
+
+// function greet(name){
+//     console.log("Hello "+ name)
+// }
+// greet("John")
+// greet("Cathy")
+
+// function greet(name,place){
+//     // console.log("My name is  "+ name +" and I am from "+place)
+//     console.log(`My name is ${name} . I am from ${place}.`)
+// }
+// greet("John","Florida")
+
+
+//write a function to accept two number and  print sum
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// sum(11,10)
+
+// function add(x,y){
+//     return x+y
+// }
+// let result=add(4,5)
+// console.log(result)
+
+//function of printing n table
+// function table(x){
+//     let i=1;
+//     for(let n=1;n<=10;n++){
+//         console.log(`${n} x ${x} = ${n*x}`)
+//     }
+// }
+// table(2)
